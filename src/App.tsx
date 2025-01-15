d7e0bb459ce3b85597fcb323ec7428cd95d0e7c0
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-01-15 23:16:16 +0530

    Enhance app functionality by integrating react-native-vector-icons and improving player setup logic with loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
+  ActivityIndicator,
   SafeAreaView,
   ScrollView,
   StatusBar,
@@ -11,7 +11,33 @@ import {
   View,
 } from 'react-native';
 
+import { setupPlayer, addTracks } from '../musicPlayerService';
+
 function App(): React.JSX.Element {
+   const [isPlayerReady, setIsPlayerReady] = useState(false);
+
+   async function setup() {
+    let isSetup = await setupPlayer();
+
+    if (isSetup) {
+      await addTracks();
+    }
+
+    setIsPlayerReady(isSetup);
+   }
+
+   useEffect(() => {
+    setup();
+   })
+
+   if (!isPlayerReady) {
+    return (
+      <SafeAreaView>
+        <ActivityIndicator />
+      </SafeAreaView>
+    );
+    }
+    
   return (
     <SafeAreaView>
       <StatusBar
@@ -23,22 +49,9 @@ function App(): React.JSX.Element {
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  container: {
+    flex: 1,
+  }
 });
 
 export default App;
