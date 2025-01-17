d546fcf1658454e13568fd12e1d0d0c01cf902f1
Abhijeet kumar <abhijeetkv2@gmail.com>
2025-01-17 23:01:28 +0530

    Update playlist data and add MusicPlayer screen component

--- src/constants.ts
+++ src/constants.ts
@@ -1,50 +1,49 @@
 import { Track } from "react-native-track-player";
 
 export const playListData: Track[] = [
-    {
-      id: 1,
-      title: 'Maan Meri Jaan',
-      artist: 'King',
-      album: 'Champagne Talk',
-      artwork:
-        'https://c.saavncdn.com/734/Champagne-Talk-Hindi-2022-20221008011951-500x500.jpg',
-      url: require('./assets/audio/one.mp3'),
-    },
-    {
-      id: 2,
-      title: 'Raataan Lambiyan',
-      artist: 'Tanishk Bagchi, Asees Kaur',
-      album: 'Shershaah',
-      artwork:
-        'https://c.saavncdn.com/238/Shershaah-Original-Motion-Picture-Soundtrack--Hindi-2021-20210815181610-500x500.jpg',
-      url: require('./assets/audio/two.mp3'),
-    },
-    {
-      id: 3,
-      title: 'Kesariya',
-      artist: 'Arijit Singh, Amitabh Bhattacharya',
-      album: 'Brahmastra',
-      artwork:
-        'https://c.saavncdn.com/191/Kesariya-From-Brahmastra-Hindi-2022-20220717092820-500x500.jpg',
-      url: require('./assets/audio/three.mp3'),
-    },
-    {
-      id: 4,
-      title: 'Title Track',
-      artist: 'Arijit Singh, Parampara Tandon',
-      album: 'Pal Pal Dil Ke Paas',
-      artwork:
-        'https://c.saavncdn.com/328/Pal-Pal-Dil-Ke-Paas-Hindi-2019-20200420150444-500x500.jpg',
-      url: require('./assets/audio/four.mp3'),
-    },
-    {
-      id: 5,
-      title: 'Besharam Rang',
-      artist: 'Vishal & Shekhar, Shilpa Rao',
-      album: 'Pathaan',
-      artwork:
-        'https://c.saavncdn.com/807/Pathaan-Hindi-2022-20221222104158-500x500.jpg',
-      url: require('./assets/audio/five.mp3'),
-    },
-  ];
-  
+  {
+    id: 1,
+    title: 'Dhaaga',
+    artist: 'TVF',
+    album: 'Originals',
+    artwork:
+      'https://c.saavncdn.com/592/Aspirants-Season-1-Music-From-the-TVF-Original-Series--English-2021-20210505184143-500x500.jpg',
+    url: require('./assets/audio/one.mp3'),
+  },
+  {
+    id: 2,
+    title: 'G.O.A.T',
+    artist: 'Diljit Dosanjh',
+    album: 'G.O.A.T',
+    artwork:
+      'https://upload.wikimedia.org/wikipedia/en/b/bb/Goat_Diljit.jpg',
+    url: require('./assets/audio/two.mp3'),
+  },
+  {
+    id: 3,
+    title: 'Get Up Jawani',
+    artist: 'Various Artists',
+    album: 'Party Hits',
+    artwork:
+      'https://i.scdn.co/image/ab67616d0000b273ca6243433c75d8ccfc39e3c4',
+    url: require('./assets/audio/three.mp3'),
+  },
+  {
+    id: 4,
+    title: 'Hass Hass',
+    artist: 'Punjabi Vibes',
+    album: 'Punjabi Beats',
+    artwork:
+      'https://i.scdn.co/image/ab67616d0000b27374a99276badeec2675a9eaec',
+    url: require('./assets/audio/four.mp3'),
+  },
+  {
+    id: 5,
+    title: 'TVF\'s Cute Vol. 1',
+    artist: 'TVF',
+    album: 'TVF Originals',
+    artwork:
+      'https://i.ytimg.com/vi/siuh55fA3N8/maxresdefault.jpg',
+    url: require('./assets/audio/five.mp3'),
+  },
+];
---
+++ src/screens/MusicPlayer.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const MusicPlayer = () => {
+  return (
+    <View>
+      <Text>MusicPlayer</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
+
+export default MusicPlayer;
